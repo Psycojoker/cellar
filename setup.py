660c63fd155f809d89150ba71ed663c1f5806ffa
Laurent Peuch <cortex@worlddomination.be>
2014-01-07 07:45:55 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='cellar',
+      version='0.1',
+      description='pkg manager for salt formulas',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/cellar',
+      install_requires=['argh'],
+      packages=[],
+      license='',
+      scripts=['cellar'],
+      keywords='salt pip pkg formula',
+      )
