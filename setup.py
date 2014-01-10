1e108778e64b921fd64dda4e8388e68df537fa56
Laurent Peuch <cortex@worlddomination.be>
2014-01-10 05:33:15 +0100

    [enh] new release \o/

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='cellar',
-      version='0.1.1',
+      version='0.1.2',
       description='pkg manager for salt formulas',
       author='Laurent Peuch',
       #long_description='',
